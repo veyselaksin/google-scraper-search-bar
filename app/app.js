29c1a221128c24e4310c994483a1d772e579a145
Veysel Aksin <veysel.aksn@gmail.com>
2022-06-18 19:16:13 +0300

    app.js refactored!

--- app/app.js
+++ app/app.js
@@ -10,8 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
 
 app.get("/", (request, response) => {
     response.sendFile(__dirname + "/views/index.html")
